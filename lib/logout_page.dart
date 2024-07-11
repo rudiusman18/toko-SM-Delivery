21089565a5fcd0d82ba2453291629fd50e5a37bf
Rudi Usman <rudiusmanprogrammer@gmail.com>
2024-07-11 21:22:10 +0700

    c

--- lib/logout_page.dart
+++ lib/logout_page.dart
@@ -5,6 +5,8 @@ class LogoutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: Colors.white,
+    );
   }
 }
